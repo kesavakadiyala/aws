d1d49a9924e50cb89caf189055a503fdbf2cb96b
= <=>
2020-09-24 21:14:17 +0530

    Instance creation/describe/terminate

---
+++ ec2-instance.sh
@@ -0,0 +1,32 @@
+#!bin/bash
+
+case $1 in
+  create|request)
+    echo -n "Enter image-id: "
+    read imageId
+    echo -n "Number of Instances: "
+    read numberOfInstances
+    echo -n "Instance Type [EX: t2.micro]: "
+    read type
+    echo -n "Security Group Id: "
+    read securityGroupId
+    aws ec2 run-instances --image-id $imageId --count $numberOfInstances --instance-type $type --security-group-ids $securityGroupId
+  ;;
+
+  describe)
+    echo -n "Enter image-id(If multiple , seperated): "
+    read imageId
+    aws ec2 describe-instances --filters "Name=image-id,Values=$imageId"
+  ;;
+
+  terminate)
+    echo -n "Instance Id: "
+    read instanceId
+    aws ec2 terminate-instances --instance-ids $instanceId
+  ;;
+
+#  tag)
+#    aws ec2 create-tags --resources i-5203422c --tags Key=Name,Value=MyInstance
+#  ;;
+
+esac
