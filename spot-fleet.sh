7950aa9b0f66421f4eb155782496dc1b78d2f6f3
= <=>
2020-09-30 16:15:04 +0530

    Fixed bugs

--- spot-fleet.sh
+++ spot-fleet.sh
@@ -1,8 +1,6 @@
 #!bin/bash
 
-if [ -z "$1" ]; then
-  echo -e "\e[1;31mPlease provide json file as input \nUsage: sh $0 specifications.json\e[0m";
-  exit 1;
-fi
-
-aws ec2 request-spot-fleet --spot-fleet-request-config file://$1
+for i in *.json; do
+  echo $i;
+  aws ec2 request-spot-fleet --spot-fleet-request-config file://$i
+done
