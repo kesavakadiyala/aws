0b060a191f6b24429c53f82854a2f0066627fa35
= <=>
2020-10-03 18:53:54 +0530

    Modified rote53

--- route53-change-resource-record-sets.sh
+++ route53-change-resource-record-sets.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-VAR="catalogue cart user redis mongodb shipping payment mysql rabbitmq"
-
-for i in $VAR ; do
+for i in *.json ; do
+  COMPONENT=$(echo $i | sed -e 's/.json//')
   IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${i}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
+  if [ -z "${IP}" ]; then
+    continue
+  fi
+  echo $IP component=$COMPONENT >> /root/hosts
   sed -e "s/IPADDRESS/${IP}/" -e "s/DNS_NAME/${i}/" record >/tmp/record.json
   aws route53 change-resource-record-sets --hosted-zone-id Z0764860BYG4XDF2MKRC --change-batch file:///tmp/record.json
 done
