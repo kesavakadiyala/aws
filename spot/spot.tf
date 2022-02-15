4ed73ce2c2142bb0be857fcce5ef65b68450907d
Kesava Kadiyala <kesavakadiyala@gmail.com>
2022-02-15 15:33:48 +0530

    Bidding for more price

--- spot/spot.tf
+++ spot/spot.tf
@@ -1,7 +1,7 @@
 resource "aws_spot_instance_request" "cheap_worker" {
   count         = length(var.apps)
   ami           = data.aws_ami.ami.id
-  spot_price    = "0.04"
+  spot_price    = "0.05"
   instance_type = "r5.large"
 }
 
